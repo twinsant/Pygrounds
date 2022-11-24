54867fa57b43e6be69f20e039bbd49da12030b56
twinsant <twinsant@gmail.com>
2022-11-24 15:47:38 +0800

    Add: antd.

--- pygrounds-app/pages/index.js
+++ pygrounds-app/pages/index.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
+import Script from 'next/script'
+import { Breadcrumb, Layout, Menu } from 'antd';
 import styles from '../styles/Home.module.css'
+
+const { Header, Content, Footer } = Layout;
 
 export default function Home() {
   useEffect(() => {
@@ -18,10 +22,51 @@ export default function Home() {
         <meta name="description" content="Best online Python playgrounds." />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/xterm.css" />
-        <script src="/xterm.js"></script>
       </Head>
+      <Script src="/xterm.js" />
 
-      <div id="terminal"/>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={new Array(15).fill(null).map((_, index) => {
+              const key = index + 1;
+              return {
+                key,
+                label: `nav ${key}`,
+              };
+            })}
+          />
+        </Header>
+        <Content
+          style={{
+            padding: '0 50px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">
+            <div id="terminal" />
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     </div>
   )
 }
