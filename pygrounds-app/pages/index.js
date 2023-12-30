1a29eb598dc30753cf3fc3ca4959e2f5a332ac96
twinsant <twinsant@gmail.com>
2023-12-30 17:06:06 +0800

    Add: local pyodide.

--- pygrounds-app/pages/index.js
+++ pygrounds-app/pages/index.js
@@ -101,7 +101,7 @@ export default function Home() {
         <title>Pygrounds</title>
         <meta name="description" content="Best online Python playgrounds." />
       </Head>
-      <Script src="https://cdn.jsdelivr.net/pyodide/v0.23.0/full/pyodide.js"
+      <Script src="/pyodide-v0.23.0/pyodide.js"
         onLoad={pyodideLoaded} 
         onError={pyodideLoadError}
       />
